d0673c4d7246324e0f301a0b4683ccea928cb803
Matthieu Girard <matg@microsoft.com>
2025-02-25 19:26:01 +0100

    fix agent scanned

--- evaluation/generate_evaluation_data.py
+++ evaluation/generate_evaluation_data.py
@@ -1,25 +1,33 @@
 import sys
 import os
 import pandas as pd
+from azure.core.settings import settings
+
+settings.tracing_implementation = "opentelemetry"
 
 # Add the root directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app, process
+from services.advice import advice
 
 dataset = pd.read_json("./evaluation/generated_datasets/insurance_simulated_dataset_fr.jsonl", lines=True).to_dict(orient="records")
 results = []
 
-with app.app_context():
-    for element in dataset:
-        response = process(user_query=element['query'], user_id="matthieu")
-        results.append({
-            "query": element['query'],
-            "response": response['content'],
-            "ground_truth": element['ground_truth']
-        })
+for element in dataset:
+    messages = [
+        { "role": "system", "content": "empty" },
+        { "role": "user", "content": element['query'] }
+    ]
+
+    response = advice(messages)
+
+    results.append({
+        "query": element['query'],
+        "response": response['advices'],
+        "ground_truth": element['ground_truth']
+    })
 
 df = pd.DataFrame(results)
-with open("evaluation/generated_datasets/insurance_evaluation_dataset.jsonl", "w") as f:
+with open("evaluation/generated_datasets/insurance_advice_evaluation_dataset.jsonl", "w") as f:
     f.write(df.to_json(orient="records", lines=True, force_ascii=False))
 
